f22d827b72ed742a0c1c222e4468a476160e9f51
nikola-golijanin <golijanin.nikola@outlook.com>
2024-01-16 19:34:49 +0100

    Login page

---
+++ WebApp_UnderTheHood/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp_UnderTheHood.Pages.Account;
+
+public class Login : PageModel
+{
+    [BindProperty] public Credential Credential { get; set; } = new Credential();
+
+    public void OnGet()
+    {
+    }
+
+    public void OnPost()
+    {
+        
+    }
+}
+
+public class Credential
+{
+    [Required]
+    [Display(Name = "User Name")]
+    public string UserName { get; set; } = string.Empty;
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; } = string.Empty;
+}
