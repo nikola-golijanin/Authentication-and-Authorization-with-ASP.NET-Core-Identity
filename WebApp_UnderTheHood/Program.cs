1b2920303daece5ccabba7980c62edc596c3898f
Nikola Golijanin <64159013+nikola-golijanin@users.noreply.github.com>
2024-01-18 12:52:15 +0100

    Simple Policy based Authorization

--- WebApp_UnderTheHood/Program.cs
+++ WebApp_UnderTheHood/Program.cs
@@ -8,6 +8,15 @@ builder.Services.AddAuthentication("MyCookieAuth").AddCookie("MyCookieAuth", opt
     options.Cookie.Name = "MyCookieAuth";
 });
 
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireClaim("Admin"));
+    options.AddPolicy("MustBelongToHRDepartment", policy => policy.RequireClaim("Department", "HR"));
+    options.AddPolicy("HRManagerOnly", policy => policy
+        .RequireClaim("Department", "HR")
+        .RequireClaim("Manager"));
+})
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
