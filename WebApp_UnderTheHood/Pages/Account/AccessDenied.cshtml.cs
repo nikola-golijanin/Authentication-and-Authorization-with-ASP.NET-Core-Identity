a1ccad615fa689a5be5448d2fcab89704855b366
Nikola Golijanin <64159013+nikola-golijanin@users.noreply.github.com>
2024-01-18 13:00:10 +0100

    Fixed typos

--- WebApp_UnderTheHood/Pages/Account/AccessDenied.cshtml.cs
+++ WebApp_UnderTheHood/Pages/Account/AccessDenied.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApp_UnderTheHood.Pages.Account
+namespace WebApp_UnderTheHood.Pages.Account;
 
 public class AccessDeniedModel : PageModel
 {
--- WebApp_UnderTheHood/Pages/HumanResource.cshtml.cs
+++ WebApp_UnderTheHood/Pages/HumanResource.cshtml.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace WebApp_UnderTheHood.Pages
+namespace WebApp_UnderTheHood.Pages;
 
 [Authorize(Policy = "MustBelongToHRDepartment")]
 public class HumanResourceModel : PageModel
